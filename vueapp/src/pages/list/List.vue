e41d319e9af3e0a08f249dad4987b62b4954fba5
星星 <244486628@qq.com>
2018-11-18 11:03:08 +0800

    1118

--- vueapp/src/pages/list/List.vue
+++ vueapp/src/pages/list/List.vue
@@ -1,15 +1,12 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div>list</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'List'
 }
 </script>
 
 <style>
-
 </style>
