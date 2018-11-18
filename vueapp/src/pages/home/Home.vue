7d3eec76cd5cafcc33800e74e7daede82ebe9e16
星星 <244486628@qq.com>
2018-11-19 04:20:59 +0800

    1119-1

--- vueapp/src/pages/home/Home.vue
+++ vueapp/src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <!-- 组件渲染 -->
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
@@ -11,12 +12,15 @@
 import HomeHeader from './components/Header'
 // 引入swiper组件
 import HomeSwiper from './components/Swiper'
+// 引入Icons组件
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   // 声明组件
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
--- vueapp/src/pages/home/components/Header.vue
+++ vueapp/src/pages/home/components/Header.vue
@@ -20,6 +20,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+
 @import '~styles/varibles.styl'
   .header
     display: flex
