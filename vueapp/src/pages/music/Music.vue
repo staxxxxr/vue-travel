0acd49a14f56a14e628fdd41cb082ae7cb9dcb37
星星 <244486628@qq.com>
2019-01-25 00:20:32 +0800

    1

--- vueapp/src/pages/music/Music.vue
+++ vueapp/src/pages/music/Music.vue
@@ -2,18 +2,22 @@
   <div>
     <!-- 子组件渲染 -->
     <music-header></music-header>
+    <music-list></music-list>
   </div>
 </template>
 
 <script>
 // 引入MusicHeader组件
 import MusicHeader from './components/Header'
+// 引入MusicHeader组件
+import MusicList from './components/Musiclist'
 
 export default {
   name: 'Music',
   // 声明组件
   components: {
-    MusicHeader
+    MusicHeader,
+    MusicList
   }
 }
 </script>
---
+++ vueapp/src/pages/music/components/Musiclist.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="musiclist">
+      <audio src="http://music.163.com/song/media/outer/url?id=460478768.mp3" controls>1</audio>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MusicList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+</style>
